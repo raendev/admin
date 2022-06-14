raendev/admin

commit ec77b159749bf3496aa7364aa20a6e393b71d95d

feat: contract-specific authentication
Rather than returning `wallet` from `near#init`, return `currentUser`.
This needs to make an RPC query to figure out if the key that NAJ stored
in localStorage is for the current contract or another, making
`currentUser` a promise (the rest of `init` is synchronous, and much of
the codebase relies on it staying that way).

src/components/Form/Form.tsx
@@ -107,7 +107,7 @@ function allFilled(formData?: FormData, required?: string[]) {
 }
 
 export function Form() {
-  const { canCall, config, wallet, getMethod, getDefinition } = useNear()
+  const { canCall, config, currentUser, getMethod, getDefinition } = useNear()
   const { contract, method } = useParams<{ contract: string, method: string }>()
   const [searchParams, setSearchParams] = useSearchParams()
   const formData = decodeData(searchParams)
@@ -121,36 +121,37 @@ export function Form() {
   const nonReactParams = window.location.search
 
   useEffect(() => {
-    if (!wallet || !config || !nonReactParams) return
+    (async () => {
+      const user = await currentUser
+      if (!user || !config || !nonReactParams) return
 
-    const params = new URLSearchParams(nonReactParams)
-    const txHash = params.get('transactionHashes') ?? undefined
-    const errMsg = params.get('errorMessage') ?? undefined
-    const errCode = params.get('errorCode') ?? undefined
+      const params = new URLSearchParams(nonReactParams)
+      const txHash = params.get('transactionHashes') ?? undefined
+      const errMsg = params.get('errorMessage') ?? undefined
+      const errCode = params.get('errorCode') ?? undefined
 
-    if (errMsg) setError(decodeURIComponent(errMsg))
-    else if (errCode) setError(decodeURIComponent(errCode))
-    else if (txHash) {
-      (async () => {
+      if (errMsg) setError(decodeURIComponent(errMsg))
+      else if (errCode) setError(decodeURIComponent(errCode))
+      else if (txHash) {
         const rpc = new JsonRpcProvider(config.nodeUrl)
-        const tx = await rpc.txStatus(txHash, wallet.getAccountId())
+        const tx = await rpc.txStatus(txHash, user.accountId)
         if (!hasSuccessValue(tx.status)) return undefined
         setResult(parseResult(tx.status.SuccessValue))
         setTx(txHash)
-      })()
-    }
-  }, [config, wallet, nonReactParams])
+      }
+    })()
+  }, [config, currentUser, nonReactParams])
 
   useEffect(() => {
     if (!method) {
       setWhyForbidden(undefined)
     } else {
       (async () => {
-        const [, why] = await canCall(method, wallet?.getAccountId())
+        const [, why] = await canCall(method, (await currentUser)?.accountId)
         setWhyForbidden(why)
       })()
     }
-  }, [canCall, method, wallet]);
+  }, [canCall, method, currentUser]);
 
   const setFormData = useMemo(() => ({ formData: newFormData }: WrappedFormData) => {
     setSearchParams(
@@ -170,17 +171,18 @@ export function Form() {
     setLoading(true)
     setError(undefined)
     setTx(undefined)
-    if (!contract || !method) return
+    const user = await currentUser
+    if (!contract || !method || !user) return
     try {
       if (getDefinition(method)?.contractMethod === 'view') {
-        const res = await wallet?.account().viewFunction(
+        const res = await user.viewFunction(
           contract,
           snake(method),
           formData?.args
         )
         setResult(JSON.stringify(res, null, 2));
       } else {
-        const res = await wallet?.account().functionCall({
+        const res = await user.functionCall({
           contractId: contract,
           methodName: snake(method),
           args: formData?.args ?? {},
@@ -210,7 +212,7 @@ export function Form() {
     } finally {
       setLoading(false)
     }
-  }, [contract, getDefinition, method, wallet])
+  }, [contract, getDefinition, method, currentUser])
 
   // update page title based on current contract & method; reset on component unmount
   useEffect(() => {
@@ -221,14 +223,17 @@ export function Form() {
 
   // at first load, auto-submit if required arguments are fill in
   useEffect(() => {
-    if (!method) return
-    const def = getDefinition(method)
-    if (def?.contractMethod === 'view' && allFilled(formData, def?.properties?.args?.required)) {
-      setTimeout(() => onSubmit({ formData }), 100)
-    }
-    // purposely only re-check this when method changes or when schema fetch completes (wallet becomes defined);
+    (async () => {
+      const user = await currentUser
+      if (!method || !user) return
+      const def = getDefinition(method)
+      if (def?.contractMethod === 'view' && allFilled(formData, def?.properties?.args?.required)) {
+        setTimeout(() => onSubmit({ formData }), 100)
+      }
+    })()
+    // purposely only re-check this when method changes or when schema fetch completes (currentUser becomes defined);
     // don't want to auto-submit while filling in form, but do when changing methods
-  }, [wallet, method]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [currentUser, method]) // eslint-disable-line react-hooks/exhaustive-deps
 
   if (!method) return null
 

src/components/Login/Login.tsx
@@ -1,29 +1,42 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { ConnectedWalletAccount } from 'near-api-js'
 import useNear from "../../hooks/useNear"
 import { Dropdown } from ".."
 import { Wallet } from './Wallet'
 import css from './login.module.css'
 
 export function Login() {
-  const { wallet, signIn, signOut } = useNear()
+  const { currentUser, signIn, signOut } = useNear()
+  const [user, setUser] = useState<ConnectedWalletAccount>()
+  const [loaded, setLoaded] = useState(false)
 
-  if (!wallet) return null
+  useEffect(() => {
+    currentUser.then(u => {
+      setUser(u)
+      setLoaded(true)
+    })
+  }, [currentUser])
 
-  const currentUser = wallet.getAccountId()
-
-  const el = currentUser
-    ? <Dropdown
-        trigger={
-          <button title={currentUser}>
-            <Wallet />
-            <span className="ellipsis">
-              {currentUser}
-            </span>
-          </button>
-        }
-        items={[{ children: "Sign Out", onSelect: signOut }]}
-      />
-    : <button onClick={signIn}><Wallet />Sign In</button>;
-
-  return <div className={css.login}>{el}</div>
+  return (
+    <div
+      className={css.login}
+      style={{ visibility: loaded ? undefined : 'hidden' }}
+    >
+      {user ? (
+        <Dropdown
+          trigger={
+            <button title={user.accountId}>
+              <Wallet />
+              <span className="ellipsis">
+                {user.accountId}
+              </span>
+            </button>
+          }
+          items={[{ children: "Sign Out", onSelect: signOut }]}
+        />
+      ) : (
+        <button onClick={signIn}><Wallet />Sign In</button>
+      )}
+    </div>
+  )
 }

src/components/Methods/Method.tsx
@@ -47,18 +47,19 @@ export const Method: React.FC<{
   contract?: string
   isCurrentMethod: boolean
 }> = ({ method, contract, isCurrentMethod }) => {
-  const { canCall, wallet } = useNear()
-  const user = wallet?.getAccountId() as string
+  const { canCall, currentUser } = useNear()
   const [allowed, setAllowed] = useState<boolean>(true)
   const [whyForbidden, setWhyForbidden] = useState<string>()
 
   useEffect(() => {
-    canCall(method, user).then(can => {
-      setAllowed(can[0])
-      setWhyForbidden(can[1] || undefined)
-    })
-  }, [method, user, canCall])
-
+    (async () => {
+      const user = await currentUser
+      canCall(method, user?.accountId).then(can => {
+        setAllowed(can[0])
+        setWhyForbidden(can[1] || undefined)
+      })
+    })()
+  }, [method, currentUser, canCall])
 
   if (isCurrentMethod) {
     return (

src/hooks/useNear.ts
@@ -15,7 +15,7 @@ const stub = {
   contract: undefined,
   config: undefined,
   near: undefined,
-  wallet: undefined,
+  currentUser: Promise.resolve(undefined),
   signIn: () => { },
   signOut: () => { },
   schema: undefined,

src/near/index.ts
@@ -37,7 +37,7 @@ export interface ContractInterface {
   contract: string,
   config: typeof testnetConfig | typeof mainnetConfig
   near: naj.Near
-  wallet: naj.WalletConnection
+  currentUser: Promise<undefined | naj.ConnectedWalletAccount>
   signIn: () => void
   signOut: () => void
 }
@@ -73,7 +73,37 @@ export function init(contract: string): ContractInterface {
 
   const wallet = new naj.WalletConnection(near)
 
+  /**
+   * Only return current user if they're authenticated against the given contract.
+   *
+   * Put another way, make sure that current browser session has a FunctionCall
+   * Access Key that allows it to call the given `contract` on behalf of the
+   * current user.
+   *
+   * @param contract The address of a NEAR contract
+   * @returns `wallet.account()` if authenticated against given `contract`
+   */
+  async function getCurrentUser(contract: string): Promise<undefined | naj.ConnectedWalletAccount> {
+    if (!wallet.getAccountId()) return undefined
+
+    const currentUser = wallet.account()
+
+    // `findAccessKey` ignores the provided `contract` and returns whatever key is in localStorage
+    const key = await currentUser.findAccessKey(contract, [])
+
+    if (!key || !key.accessKey) return undefined
+
+    // FullAccess keys don't provide info about what contract they target, so we
+    // need to assume the worst. This should never happen for this app, though.
+    if (key.accessKey.permission === 'FullAccess') return undefined
+
+    return contract === key.accessKey.permission.FunctionCall.receiver_id
+      ? currentUser
+      : undefined
+  }
+
   function signIn() {
+    wallet.signOut()
     wallet.requestSignIn({ contractId: contract })
   }
 
@@ -82,7 +112,15 @@ export function init(contract: string): ContractInterface {
     window.location.reload()
   }
 
-  cache[contract] = { contract, config, near, wallet, signIn, signOut }
+  cache[contract] = {
+    contract,
+    config,
+    near,
+    currentUser: getCurrentUser(contract),
+    signIn,
+    signOut,
+  }
 
   return cache[contract]
 }
+

src/near/schema.ts
@@ -143,7 +143,7 @@ export interface SchemaInterface {
    * @param account string Account name that may or may not be allowed to call `method` on `contract`
    * @returns [boolean, string] boolean is true of `account` can call `method` on `contract`, false if not. string contains reason why user is forbidden.
    */
-  canCall: (method: string, account: string) => Promise<readonly [boolean, string | undefined]>
+  canCall: (method: string, account?: string) => Promise<readonly [boolean, string | undefined]>
 }
 
 type InMemoryCachedSchema = SchemaInterface & {
@@ -220,16 +220,16 @@ function buildInterface(contract: string, schema: JSONSchema7): SchemaInterface 
     return def as MethodDefinition
   }
 
-  async function canCall(method: string, account: string): Promise<readonly [false, string]>;
-  async function canCall(method: string, account: string): Promise<readonly [true, undefined]>;
-  async function canCall(method: string, account: string): Promise<readonly [boolean, string | undefined]> {
+  async function canCall(method: string, account?: string): Promise<readonly [false, string]>;
+  async function canCall(method: string, account?: string): Promise<readonly [true, undefined]>;
+  async function canCall(method: string, account?: string): Promise<readonly [boolean, string | undefined]> {
     const def = getDefinition(method)
 
-    if (!def) return [false, `No method "${method}" exists for contract "${contract}"`]
+    if (!def || !def.contractMethod) return [false, `No method "${method}" exists for contract "${contract}"`]
 
+    if (def.contractMethod === 'view') return [true, undefined]
 
-    if (def.contractMethod === 'change' && !account) return [false, 'Must sign in']
-
+    if (!account) return [false, 'Must sign in']
 
     const hasField = hasAllowProperty(def)
 

commit f30f2da2addeb2f1ee3f0733a56ed562de05179b

fix: submit view calls when signed out
This broke with ec77b159749bf3496aa7364aa20a6e393b71d95d

src/components/Form/Form.tsx
@@ -107,7 +107,7 @@ function allFilled(formData?: FormData, required?: string[]) {
 }
 
 export function Form() {
-  const { canCall, config, currentUser, getMethod, getDefinition } = useNear()
+  const { near, canCall, config, currentUser, getMethod, getDefinition } = useNear()
   const { contract, method } = useParams<{ contract: string, method: string }>()
   const [searchParams, setSearchParams] = useSearchParams()
   const formData = decodeData(searchParams)
@@ -171,17 +171,19 @@ export function Form() {
     setLoading(true)
     setError(undefined)
     setTx(undefined)
-    const user = await currentUser
-    if (!contract || !method || !user) return
+    if (!near || !contract || !method) return
     try {
       if (getDefinition(method)?.contractMethod === 'view') {
-        const res = await user.viewFunction(
+        const account = await near.account(contract)
+        const res = await account.viewFunction(
           contract,
           snake(method),
           formData?.args
         )
         setResult(JSON.stringify(res, null, 2));
       } else {
+        const user = await currentUser
+        if (!user) throw new Error('Forbidden: must sign in')
         const res = await user.functionCall({
           contractId: contract,
           methodName: snake(method),
@@ -212,7 +214,7 @@ export function Form() {
     } finally {
       setLoading(false)
     }
-  }, [contract, getDefinition, method, currentUser])
+  }, [near, contract, getDefinition, method, currentUser])
 
   // update page title based on current contract & method; reset on component unmount
   useEffect(() => {
@@ -223,17 +225,16 @@ export function Form() {
 
   // at first load, auto-submit if required arguments are fill in
   useEffect(() => {
+    if (!method || !schema) return
     (async () => {
-      const user = await currentUser
-      if (!method || !user) return
       const def = getDefinition(method)
       if (def?.contractMethod === 'view' && allFilled(formData, def?.properties?.args?.required)) {
         setTimeout(() => onSubmit({ formData }), 100)
       }
     })()
-    // purposely only re-check this when method changes or when schema fetch completes (currentUser becomes defined);
+    // purposely only re-check this when method changes or when schema fetch completes;
     // don't want to auto-submit while filling in form, but do when changing methods
-  }, [currentUser, method]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [schema, method]) // eslint-disable-line react-hooks/exhaustive-deps
 
   if (!method) return null
 