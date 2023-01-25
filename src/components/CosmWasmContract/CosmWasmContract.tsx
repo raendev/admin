1feadc9fa083390793a046a705b081a87ecfe4aa
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-01-25 14:59:06 -0500

    add example CosmWasm schema

--- src/components/CosmWasmContract/CosmWasmContract.tsx
+++ src/components/CosmWasmContract/CosmWasmContract.tsx
@@ -1,7 +1,8 @@
 import { Layout } from ".."
+import schema from '../../protocols/cw/cw20-ics20.json';
 
 export function CosmWasmContract() {
   return <Layout showLogin={false}>
-    <h1>CosmWasmContract</h1>
+    <h1>{schema.contract_name} {schema.contract_version}</h1>
   </Layout>
 }
